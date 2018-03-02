459de803867bbb6ac636ed71cb3ccfc653817325
Brent Guistwite <brentguistwite@gmail.com>
2018-03-02 14:19:46 -0500

    add second solution

--- palindromes.js
+++ palindromes.js
@@ -1,26 +1,36 @@
 const stack = require('./Stack.js');
 
+// -----------V1---------------
+// function is_palindrome(string) {
+//   string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+//   const newStack = new stack();
+//   for (let i = 0; i < string.length; i++){
+//     newStack.push(string[i]);
+//   }
+//   let x = '';
+//   for (let i = 0; i < string.length; i++){
+//     x += newStack.pop();
+//   }
+//   return x === string; 
+// }
 
+// -----------V2------------------
 function is_palindrome(string) {
   string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
   const newStack = new stack();
-  for (let i = 0; i < string.length; i++){
+  for (let i = 0; i < string.length; i++) {
     newStack.push(string[i]);
   }
-  let x = '';
-  for (let i = 0; i < string.length; i++){
-    x += newStack.pop();
+  for (let i = 0; i < string.length / 2; i++){
+    if (string[i] !== newStack.pop()){ 
+      return false;
+    }
   }
-  return x === string; 
+  return true;
 }
 
 
-// if string length % 2 === 1
-// s[0] === stack.pop()
-// for(i=0; i<s.length/2; i++){
-// if (s[i] !== stack.pop()) return false
-// "amanaplana c analpanama" = 21 length
 // true, true, true
-console.log(is_palindrome("A man, a plan, a canal: Panama"));
-// console.log(is_palindrome("1001"));
-// console.log(is_palindrome("Tauhida"));
+console.log(is_palindrome('A man, a plan, a canal: Panama'));
+console.log(is_palindrome("10121121201"));
+console.log(is_palindrome("Tauhida"));
